352cca934b81c662e06aa3eb7a44fc7b278389b4
Gláucio <glauciofilho@live.com>
2025-03-16 17:46:40 -0300

    Taking out the docker-compose and add /health

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+import jsonify
 
 main = Blueprint("main", __name__)
 
@@ -9,3 +10,9 @@ def home():
 @main.route("/projects")
 def projects():
     return render_template("projects.html")
+
+@main.route('/health')
+def health_check():
+    """Endpoint for health checks."""
+
+    return jsonify({"status": "ok"}), 200
